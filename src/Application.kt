87df17ae3f9fe334fc0ab1e87afc883cfaefb1db
Muh. Rahmatullah <muh.rahmatullah@grabtaxi.com>
2020-02-29 23:33:47 +0700

    init commit 👨‍💻

---
+++ src/Application.kt
@@ -0,0 +1,33 @@
+package dev.rahmat
+
+import io.ktor.application.Application
+
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
